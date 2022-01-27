d52f26c1c2acbc3fb84b3ca7c9ef5bf332c93130
cmuhoza <ceddymuhoza@gmail.com>
2022-01-27 18:54:31 -0500

    export resolvers

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,9 @@
-export * from './chat';
-export * from './roadman';
+import {CommentDefaultResolver} from './comment/Comment.model';
+import {PostDefaultResolver} from './post/Post.model';
+import {ReactionDefaultResolver} from './reaction/Reaction.model';
+
+export const getSocialResolvers = () => [
+    CommentDefaultResolver,
+    PostDefaultResolver,
+    ReactionDefaultResolver,
+];
