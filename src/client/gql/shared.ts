c976eace6b5d2cc08af3c1effbb9a3ef171c83ba
cmuhoza <ceddymuhoza@gmail.com>
2022-02-03 11:18:53 -0500

    SocialResType

--- src/client/gql/shared.ts
+++ src/client/gql/shared.ts
@@ -1,13 +1,13 @@
 import gql from 'graphql-tag';
 
-export interface ResType {
+export interface SocialResType {
     success: boolean;
     message?: string;
     data?: any;
 }
 
 export const SocialResTypeFragment = gql`
-    fragment SocialResTypeFragment on ResType {
+    fragment SocialResTypeFragment on SocialResType {
         success
         message
         data
