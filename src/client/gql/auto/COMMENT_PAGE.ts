f20673158419aa9d4b3ce987a0fba2e145ab9653
cmuhoza <ceddymuhoza@gmail.com>
2022-02-23 23:55:21 -0500

    Add auto clients for autogenerate code

--- src/client/gql/auto/COMMENT_PAGE.ts
+++ src/client/gql/auto/COMMENT_PAGE.ts
@@ -1,1 +1,86 @@
-export const CommentPAGE = {"kind":"Document","definitions":[{"kind":"OperationDefinition","operation":"query","name":{"kind":"Name","value":"PageComment"},"variableDefinitions":[{"kind":"VariableDefinition","variable":{"kind":"Variable","name":{"kind":"Name","value":"id"}},"type":{"kind":"NonNullType","type":{"kind":"NamedType","name":{"kind":"Name","value":"String"}}},"directives":[]}],"directives":[],"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","alias":{"kind":"Name","value":"data"},"name":{"kind":"Name","value":"commentPagination"},"arguments":[{"kind":"Argument","name":{"kind":"Name","value":"id"},"value":{"kind":"Variable","name":{"kind":"Name","value":"id"}}}],"directives":[],"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"FragmentSpread","name":{"kind":"Name","value":"CommentFragment"},"directives":[]}]}}]}},{"kind":"FragmentDefinition","name":{"kind":"Name","value":"CommentFragment"},"typeCondition":{"kind":"NamedType","name":{"kind":"Name","value":"Comment"}},"directives":[],"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"owner"},"arguments":[],"directives":[]},{"kind":"Field","name":{"kind":"Name","value":"text"},"arguments":[],"directives":[]},{"kind":"Field","name":{"kind":"Name","value":"parentId"},"arguments":[],"directives":[]},{"kind":"Field","name":{"kind":"Name","value":"editedAt"},"arguments":[],"directives":[]}]}}],"loc":{"start":0,"end":224}};
+export const CommentPAGE = {
+    kind: 'Document',
+    definitions: [
+        {
+            kind: 'OperationDefinition',
+            operation: 'query',
+            name: {kind: 'Name', value: 'PageComment'},
+            variableDefinitions: [
+                {
+                    kind: 'VariableDefinition',
+                    variable: {kind: 'Variable', name: {kind: 'Name', value: 'id'}},
+                    type: {
+                        kind: 'NonNullType',
+                        type: {kind: 'NamedType', name: {kind: 'Name', value: 'String'}},
+                    },
+                    directives: [],
+                },
+            ],
+            directives: [],
+            selectionSet: {
+                kind: 'SelectionSet',
+                selections: [
+                    {
+                        kind: 'Field',
+                        alias: {kind: 'Name', value: 'data'},
+                        name: {kind: 'Name', value: 'commentPagination'},
+                        arguments: [
+                            {
+                                kind: 'Argument',
+                                name: {kind: 'Name', value: 'id'},
+                                value: {kind: 'Variable', name: {kind: 'Name', value: 'id'}},
+                            },
+                        ],
+                        directives: [],
+                        selectionSet: {
+                            kind: 'SelectionSet',
+                            selections: [
+                                {
+                                    kind: 'FragmentSpread',
+                                    name: {kind: 'Name', value: 'CommentFragment'},
+                                    directives: [],
+                                },
+                            ],
+                        },
+                    },
+                ],
+            },
+        },
+        {
+            kind: 'FragmentDefinition',
+            name: {kind: 'Name', value: 'CommentFragment'},
+            typeCondition: {kind: 'NamedType', name: {kind: 'Name', value: 'Comment'}},
+            directives: [],
+            selectionSet: {
+                kind: 'SelectionSet',
+                selections: [
+                    {
+                        kind: 'Field',
+                        name: {kind: 'Name', value: 'owner'},
+                        arguments: [],
+                        directives: [],
+                    },
+                    {
+                        kind: 'Field',
+                        name: {kind: 'Name', value: 'text'},
+                        arguments: [],
+                        directives: [],
+                    },
+                    {
+                        kind: 'Field',
+                        name: {kind: 'Name', value: 'parentId'},
+                        arguments: [],
+                        directives: [],
+                    },
+                    {
+                        kind: 'Field',
+                        name: {kind: 'Name', value: 'editedAt'},
+                        arguments: [],
+                        directives: [],
+                    },
+                ],
+            },
+        },
+    ],
+    loc: {start: 0, end: 224},
+};
